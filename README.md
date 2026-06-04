0d3f090f125b513409356a58e6c003c1a9899b05
Erik Westrup <erik.westrup@icloud.com>
2026-06-04 12:26:09 +0200

    Configure server host

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # gomemo
+
 A small note taking app written in Go
+
+# Development
+
+Run the server with
+
+```bash
+go run ./cmd/gomemo
+PORT=8080 go run ./cmd/gomemo
+```
+
+or with live reload:
+
+```bash
+go tool air
+```
--- internal/config/config.go
+++ internal/config/config.go
@@ -6,10 +6,16 @@ import (
 )
 
 type Config struct {
+	Host string
 	Port string
 }
 
 func Load() Config {
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "127.0.0.1"
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -17,6 +23,7 @@ func Load() Config {
 
 	return Config{
 		Port: port,
+		Host: host,
 	}
 }
 
@@ -26,5 +33,5 @@ func (cfg Config) String() string {
 }
 
 func (cfg Config) AddrString() string {
-	return fmt.Sprintf(":%s", cfg.Port)
+	return fmt.Sprintf("%s:%s", cfg.Host, cfg.Port)
 }
