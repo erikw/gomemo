c33c89bd29009e4371a2c51c6bf9c972de61fdb1
Erik Westrup <erik.westrup@icloud.com>
2026-06-04 12:17:27 +0200

    Respond with JSON

--- cmd/gomemo/main.go
+++ cmd/gomemo/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/erikw/gomemo/internal/config"
 	"github.com/go-chi/chi/v5"
@@ -16,15 +18,32 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Timeout(20 * time.Second))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("Hello, world"))
-		if err != nil {
-			// TODO use structured logging pkg
-			fmt.Printf("Error serving request: %v\n", err)
-		}
+
+		// fmt.Println(middleware.GetReqID(r.Context()))
+		// _, err := w.Write([]byte("Hello, world"))
+		// if err != nil {
+		//	// TODO use structured logging pkg
+		//	fmt.Printf("Error serving request: %v\n", err)
+		// }
+		respondJSON(w, http.StatusOK, map[string]string{
+			"status": "ok",
+		})
 	})
 	err := http.ListenAndServe(cfg.AddrString(), r)
 	if err != nil {
 		fmt.Printf("Error serving: %v\n", err)
+	}
+}
+
+func respondJSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
