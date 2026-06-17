abd5a57be7201c39e0619a78e3dfe9f97d568a92
Erik Westrup <erik.westrup@icloud.com>
2026-06-17 14:55:01 +0200

    Helper for extract notesID

--- internal/notes/handler.go
+++ internal/notes/handler.go
@@ -24,11 +24,13 @@ func NewHandler(logger *slog.Logger, service *Service) *Handler {
 
 func (h *Handler) RegisterRoutes(r chi.Router) {
 	r.Get("/notes", h.HandleGetAll) // TODO pagingate
+	// r.Post("/notes", h.HandlePost) // TODO pagingate
 	r.Get("/notes/{noteID}", h.HandleGetByID)
 	r.Delete("/notes/{noteID}", h.HandleDeleteByID)
 	// TODO
-	// POST   /notes
+	// PATCH /notes/{id}
 }
+
 func (h *Handler) HandleGetAll(w http.ResponseWriter, req *http.Request) {
 
 	notes, err := h.service.GetAll(req.Context())
@@ -42,6 +44,20 @@ func (h *Handler) HandleGetAll(w http.ResponseWriter, req *http.Request) {
 		h.logger.Error("could not respond with JSON encoding", "notes", notes)
 	}
 }
+
+// func (h *Handler) HandlePost(w http.ResponseWriter, req *http.Request) {
+//
+// 	notes, err := h.service.GetAll(req.Context())
+// 	if err != nil {
+// 		h.logger.Error(fmt.Sprintf("could not fetch Notes"))
+// 		httpx.RespondError(w, http.StatusNotFound, "Note could not be found")
+// 		return
+// 	}
+//
+// 	if err = httpx.RespondJSON(w, http.StatusOK, notes); err != nil {
+// 		h.logger.Error("could not respond with JSON encoding", "notes", notes)
+// 	}
+// }
 
 func (h *Handler) HandleGetByID(w http.ResponseWriter, req *http.Request) {
 	idStr := chi.URLParam(req, "noteID")
@@ -64,13 +80,20 @@ func (h *Handler) HandleGetByID(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func (h *Handler) HandleDeleteByID(w http.ResponseWriter, req *http.Request) {
-	// TODO extract to helper
+func (h *Handler) idFromParam(w http.ResponseWriter, req *http.Request) (int64, error) {
 	idStr := chi.URLParam(req, "noteID")
 	id, err := strconv.ParseInt(idStr, 10, 64)
 	if err != nil {
 		h.logger.Error(fmt.Sprintf("could not convert urlParam `%s` noteID to int64", idStr))
 		httpx.RespondError(w, http.StatusBadRequest, "Invalid notesID")
+		return -1, fmt.Errorf("could not extract Note ID from URL query parameters")
+	}
+	return id, nil
+}
+
+func (h *Handler) HandleDeleteByID(w http.ResponseWriter, req *http.Request) {
+	id, err := h.idFromParam(w, req)
+	if err != nil {
 		return
 	}
 
